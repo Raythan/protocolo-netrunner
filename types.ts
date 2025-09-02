958b8990a3e60a8ea92e4165528c9668110956af
Raythan Padovani Abreu Machado <raythan100@yahoo.com.br>
2025-09-02 12:49:03 -0300

    rollback

--- types.ts
+++
@@ -1,9 +0,0 @@
-export type Frequency = 'once' | 'daily' | 'weekly' | 'monthly';
-
-export interface ScheduledMessage {
-  id: string;
-  phone: string;
-  message: string;
-  scheduleTime: Date;
-  frequency: Frequency;
-}
